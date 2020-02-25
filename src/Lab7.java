d2a31b843f8976c6441521e2051c1e1da4d13590
kmarkley <kmarkley@students.kennesaw.edu>
2020-02-24 19:56:10 -0500

    Finished Lab 7.

--- src/Lab7.java
+++ src/Lab7.java
@@ -2,7 +2,49 @@ public class Lab7
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello from GIT!");
-		System.out.println("Hello from GIT again!");
+//		System.out.println("Hello from GIT!");
+//		System.out.println("Hello from GIT again!");
+		
+		String s = "Hello World!";
+		s = s.toUpperCase();
+		
+		String s2 = "aererereragfdkdfgdmgi";
+		s2 = s2.toUpperCase();
+		
+		System.out.println("Number of vowels: " + 
+		RecursiveVowelChecker(s));
+		
+		System.out.println("Number of vowels: " + 
+				RecursiveVowelChecker(s2));
+	}
+	
+	public static int RecursiveVowelChecker(String input)
+	{
+		int vowels = 0;
+		
+		if(input.length() <= 0)
+		{
+			return vowels;
+		}
+		
+		if(input.charAt(0) == 'A' || input.charAt(0) == 'E' 
+				|| input.charAt(0) == 'I' || input.charAt(0) == 'O'
+				|| input.charAt(0) == 'U' || input.charAt(0) == 'Y')
+		{
+			vowels++;
+		}
+		
+		return RecursiveVowelChecker( input.substring(1) ) + vowels;
 	}
 }
+
+//int amountOfVowels = 0;
+//for(int i = 0; i < s.length(); i++)
+//{
+//	if(s.charAt(i) == 'A' || s.charAt(i) == 'E' 
+//			|| s.charAt(i) == 'I' || s.charAt(i) == 'O'
+//			|| s.charAt(i) == 'U' || s.charAt(i) == 'Y')
+//	{
+//		amountOfVowels++;
+//	}
+//}
